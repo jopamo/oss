79c1619d630af8e373cb6d4ceb9704e4048949da
jopamo <p@1g4.org>
2024-02-22 23:45:24 -0500

    shared.c: add comments

--- shared.c
+++ shared.c
@@ -1,6 +1,13 @@
 #include "shared.h"
 
+/**
+ * Generates a System V IPC key for shared memory access, utilizes ftok
+ * to generate a unique key based on a pathname and project identifier.
+ *
+ * @return A key_t value of generated key
+ */
 key_t getSharedMemoryKey() {
+    // Generate and return a unique key for shared memory access
     return ftok(SHM_PATH, SHM_PROJ_ID);
 }
 
