af4e6db1b29d4dc5d90b881d61b36f67b6ee42bd
jopamo <p@1g4.org>
2024-02-22 20:54:58 -0500

    worker: rework closer to spec, remove sleep etc

--- worker.c
+++ worker.c
@@ -1,65 +1,48 @@
-#include "shared.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <time.h>
 
-#define NANOSECOND 10000000
+#include "shared.h"
 
-void printUsage(const char *programName) {
-  printf("Usage: %s seconds nanoseconds\n", programName);
-  printf("Example: %s 5 500000\n", programName);
-  printf("Attaches to shared memory, reads the simulated system clock, and determines a termination time. Periodically checks the system clock and terminates once the specified time has elapsed.\n");
-}
+void printUsage(const char * programName);
+void processArgs(int argc, char * argv[], int * addSeconds, int * addNanoseconds);
 
-void processArgs(int argc, char *argv[], int *addSeconds, int *addNanoseconds) {
-  if (argc != 3) {
-    fprintf(stderr, "Error: Incorrect number of arguments.\n");
-    printUsage(argv[0]);
-    exit(EXIT_FAILURE);
-  }
+int main(int argc, char * argv[]) {
+  int addSeconds, addNanoseconds;
+  processArgs(argc, argv, & addSeconds, & addNanoseconds);
 
-  *addSeconds = atoi(argv[1]);
-  *addNanoseconds = atoi(argv[2]);
-
-  if ( *addSeconds < 0 || *addNanoseconds < 0 || *addNanoseconds >= NANOSECOND) {
-    fprintf(stderr, "Error: Invalid arguments. Seconds must be >= 0 and nanoseconds in [0, 999999999].\n");
-    exit(EXIT_FAILURE);
-  }
-}
-
-void verifySharedMemory(int addSeconds, int addNanoseconds) {
   key_t key = getSharedMemoryKey();
   int shmId = shmget(key, sizeof(SimulatedClock), 0666);
   if (shmId < 0) {
-    perror("shmget failed");
+    perror("shmget");
     exit(EXIT_FAILURE);
   }
 
-  SimulatedClock *simClock = (SimulatedClock *) shmat(shmId, NULL, 0);
-  if (simClock == (void *) - 1) {
-    perror("shmat failed");
+  SimulatedClock * simClock = (SimulatedClock * ) shmat(shmId, NULL, 0);
+  if (simClock == (void * ) - 1) {
+    perror("shmat");
     exit(EXIT_FAILURE);
   }
 
-  printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d --Just Starting\n",
-    getpid(), getppid(), simClock -> seconds, simClock -> nanoseconds);
+  long long termTimeS = simClock -> seconds + addSeconds;
+  long long termTimeN = simClock -> nanoseconds + addNanoseconds;
+  while (termTimeN >= 1000000000) {
+    termTimeN -= 1000000000;
+    termTimeS += 1;
+  }
 
-  long long startSeconds = simClock -> seconds;
-  long long startNanoseconds = simClock -> nanoseconds;
-  long long totalNanoseconds = (long long) simClock -> nanoseconds + addNanoseconds;
-  int termTimeS = simClock -> seconds + addSeconds + totalNanoseconds / NANOSECOND;
-  int termTimeN = totalNanoseconds % NANOSECOND;
+  printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %lld TermTimeNano: %lld --Just Starting\n",
+    getpid(), getppid(), simClock -> seconds, simClock -> nanoseconds, termTimeS, termTimeN);
 
-  int lastElapsedSecond = -1;
-
+  long long lastReportedSecond = simClock -> seconds - 1;
   while (simClock -> seconds < termTimeS || (simClock -> seconds == termTimeS && simClock -> nanoseconds < termTimeN)) {
-    long long currentTotalNanoseconds = (simClock -> seconds - startSeconds) *NANOSECOND + (simClock -> nanoseconds - startNanoseconds);
-    int elapsedSeconds = currentTotalNanoseconds / NANOSECOND;
-
-    if (elapsedSeconds > lastElapsedSecond) {
-      printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d --%d seconds have passed since starting\n",
-        getpid(), getppid(), simClock -> seconds, simClock -> nanoseconds, elapsedSeconds);
-      lastElapsedSecond = elapsedSeconds;
+    if (simClock -> seconds > lastReportedSecond) {
+      printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d --%lld seconds have passed since starting\n",
+        getpid(), getppid(), simClock -> seconds, simClock -> nanoseconds, simClock -> seconds - (termTimeS - addSeconds));
+      lastReportedSecond = simClock -> seconds;
     }
   }
 
@@ -67,15 +50,33 @@ void verifySharedMemory(int addSeconds, int addNanoseconds) {
     getpid(), getppid(), simClock -> seconds, simClock -> nanoseconds);
 
   if (shmdt(simClock) == -1) {
-    perror("shmdt failed");
+    perror("shmdt");
     exit(EXIT_FAILURE);
   }
+
+  return 0;
 }
 
-int main(int argc, char *argv[]) {
-  int addSeconds, addNanoseconds;
+void printUsage(const char * programName) {
+  fprintf(stderr, "Usage: %s <seconds> <nanoseconds>\n", programName);
+  fprintf(stderr, "Both <seconds> and <nanoseconds> must be non-negative integers.\n");
+}
 
-  processArgs(argc, argv, & addSeconds, & addNanoseconds);
-  verifySharedMemory(addSeconds, addNanoseconds);
-  return 0;
+void processArgs(int argc, char * argv[], int * addSeconds, int * addNanoseconds) {
+  if (argc != 3) {
+    printUsage(argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  long long seconds = atoll(argv[1]);
+  long long nanoseconds = atoll(argv[2]);
+
+  if (seconds < 0 || nanoseconds < 0 || nanoseconds >= 1000000000) {
+    fprintf(stderr, "Error: Invalid time values.\n");
+    printUsage(argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  *addSeconds = (int) seconds;
+  *addNanoseconds = (int) nanoseconds;
 }
