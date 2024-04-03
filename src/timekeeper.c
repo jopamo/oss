c6afce72c17361b4373f96772e87d57a5c47f8fb
jopamo <p@1g4.org>
2024-04-03 08:00:45 -0400

    timekeeper: add speedFactor toggle on simClock

--- src/timekeeper.c
+++ src/timekeeper.c
@@ -10,25 +10,30 @@ void runTimekeeper() {
     exit(EXIT_FAILURE);
   }
 
-  clockSem = sem_open(clockSemName, 0);
+  clockSem = sem_open(clockSemName, O_CREAT | O_RDWR, 0644, 1);
   if (clockSem == SEM_FAILED) {
-    log_message(LOG_LEVEL_ERROR, "Failed to open semaphore.");
+    log_message(LOG_LEVEL_ERROR, "Failed to open semaphore: %s",
+                strerror(errno));
     exit(EXIT_FAILURE);
   }
 
+  double speedFactor = 0.28;
+  long incrementPerCycle = (long)(speedFactor * NANOSECONDS_IN_SECOND);
+
   while (keepRunning) {
-    sleep(1);
+    usleep(250000);
 
     sem_wait(clockSem);
-    unsigned int increment = 250;
-    simClock->nanoseconds += increment * 1000000L;
+    simClock->nanoseconds += incrementPerCycle;
+
     if (simClock->nanoseconds >= NANOSECONDS_IN_SECOND) {
       simClock->seconds += simClock->nanoseconds / NANOSECONDS_IN_SECOND;
       simClock->nanoseconds %= NANOSECONDS_IN_SECOND;
     }
+
     sem_post(clockSem);
 
-    log_message(LOG_LEVEL_DEBUG, "Time updated: %lu sec, %lu nsec",
+    log_message(LOG_LEVEL_DEBUG, "Simulated Time updated: %lu sec, %lu nsec",
                 simClock->seconds, simClock->nanoseconds);
   }
 }
