4c19d9d832ac268cd2bc85a3a6c465aa9ba0f4e8
jopamo <p@1g4.org>
2024-04-06 01:32:27 -0400

    add ability to kill zombie processes in clean.sh

--- clean.sh
+++ clean.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# List shared memory segments with their IDs
+ps -ef | grep 'oss' | awk '{print $2}' | xargs kill -9
+ps -ef | grep 'worker' | awk '{print $2}' | xargs kill -9
+ps -ef | grep 'timekeeper' | awk '{print $2}' | xargs kill -9
+
 segments=$(ipcs -m | awk '/^0x/ {print $2}')
 
-# Remove each segment
 for seg in $segments; do
-    ipcrm -m "$seg"
-    echo "Removed shared memory segment: $seg"
+	ipcrm -m "$seg"
+	echo "Removed shared memory segment: $seg"
 done
 
 echo "All shared memory segments removed."
